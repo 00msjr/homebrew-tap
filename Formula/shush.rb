974c1fd2dd3f775edc53ff6f4e6f6b3cbf313834
00msjr <michaelsousajr@gmail.com>
2025-04-17 14:17:15 -0400

    shush

--- Formula/shush.rb
+++ Formula/shush.rb
@@ -1,8 +1,8 @@
 class Shush < Formula
   desc "Utility for suppressing and managing output from commands and scripts"
   homepage "https://github.com/00msjr/shush"
-  url "https://github.com/00msjr/shush/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "bcf177fe56e5dcc6f3c58199a4b0dd289b0c65bfe5e9c4f9dd29c7bfab50cf2a"
+  url "https://github.com/00msjr/shush/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "e736b06a16fa62c5b69fa3b75db001dd21f3c141ecb367ea3df973d1f895c1d8"
   license "MIT"
 
   def install
